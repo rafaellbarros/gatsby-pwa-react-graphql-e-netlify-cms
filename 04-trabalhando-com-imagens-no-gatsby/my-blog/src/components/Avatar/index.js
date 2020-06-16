5b0b680006c4b793398518601546804e35ded8ba
Rafael Barros <rafaelbarros.df@gmail.com>
2020-06-16 13:21:24 -0300

    :construction: Entendendo os 2 tipos de imagem responsiva (fluid e fixed)

--- 04-trabalhando-com-imagens-no-gatsby/my-blog/src/components/Avatar/index.js
+++ 04-trabalhando-com-imagens-no-gatsby/my-blog/src/components/Avatar/index.js
@@ -8,8 +8,8 @@ const Avatar = () => {
             query {
                 avatarImage: file(relativePath: { eq: "profile-photo.png"}) {
                     childImageSharp {
-                        fixed(width: 60, height: 60) {
-                            ...GatsbyImageSharpFixed_tracedSVG
+                        fluid(maxWidth: 60, maxHeight: 60) {
+                            ...GatsbyImageSharpFluid_tracedSVG
                         }
                     }
                 }
@@ -17,7 +17,7 @@ const Avatar = () => {
         `
     )
 
-    return <Img fixed={avatarImage.childImageSharp.fixed} className="foo" />
+    return <Img fluid={avatarImage.childImageSharp.fluid} className="foo" style={{width: "60px"}}/>
 }
 
 export default Avatar
