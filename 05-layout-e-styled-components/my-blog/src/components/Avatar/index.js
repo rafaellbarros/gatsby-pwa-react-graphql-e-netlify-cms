de4a1ac6c0ba49f6392fbf804899ab8a1e7f6abd
Rafael Barros <rafaelbarros.df@gmail.com>
2020-06-16 20:44:15 -0300

    :truck: Movendo projeto

---
+++ 05-layout-e-styled-components/my-blog/src/components/Avatar/index.js
@@ -0,0 +1,23 @@
+import { graphql, useStaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
+import React from 'react'
+
+const Avatar = () => {
+    const { avatarImage } = useStaticQuery(
+        graphql`
+            query {
+                avatarImage: file(relativePath: { eq: "profile-photo.png"}) {
+                    childImageSharp {
+                        fluid(maxWidth: 60, maxHeight: 60) {
+                            ...GatsbyImageSharpFluid_tracedSVG
+                        }
+                    }
+                }
+            }
+        `
+    )
+
+    return <Img fluid={avatarImage.childImageSharp.fluid} className="foo" style={{width: "60px"}}/>
+}
+
+export default Avatar
