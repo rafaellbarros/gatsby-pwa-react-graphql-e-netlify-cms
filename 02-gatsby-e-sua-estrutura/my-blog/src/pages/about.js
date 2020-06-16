50dfcc86a672a60e365304524b407bdaf630d584
Rafael Barros <rafaelbarros.df@gmail.com>
2020-06-16 03:09:53 -0300

    :construction: Entendendo como o gatsby-link funciona

--- 02-gatsby-e-sua-estrutura/my-blog/src/pages/about.js
+++ 02-gatsby-e-sua-estrutura/my-blog/src/pages/about.js
@@ -1,7 +1,21 @@
+import { Link } from 'gatsby'
 import React from 'react'
 
 const AboutPage = () => (
+    <>
     <h1>About Page</h1>
+    <ul>
+    <li>
+      <a href="/about">About</a>
+    </li>
+    <li>
+      <Link to="/" activeStyle={{color: 'red'}}>Home</Link>
+    </li>
+    <li>
+      <Link to="/about" activeStyle={{color: 'red'}}>About (Gatsby Link)</Link>
+    </li>
+    </ul>
+  </>
 )
 
 export default AboutPage
--- 02-gatsby-e-sua-estrutura/my-blog/src/pages/index.js
+++ 02-gatsby-e-sua-estrutura/my-blog/src/pages/index.js
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby'
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -7,6 +8,17 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Home</h1>
+    <ul>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <Link to="/" activeStyle={{color: 'red'}}>Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About (Gatsby Link)</Link>
+      </li>
+    </ul>
   </Layout>
 )
 
