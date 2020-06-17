f49a9600a88db9baa7f9890f99fd54ffd76a6cab
Rafael Barros <rafaelbarros.df@gmail.com>
2020-06-17 02:27:17 -0300

    :construction: Criando o SocialLinks Component - Parte 1

---
+++ 05-layout-e-styled-components/my-blog/src/components/SocialLinks/content.js
@@ -0,0 +1,23 @@
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/rafaellbarros`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/_rafaelk99`,
+  },
+  {
+    label: "Youtube",
+    url: `https://www.youtube.com/xpto`,
+  },
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/_rafaelk99`,
+  },
+  {
+    label: "Unsplash",
+    url: `https://unsplash.com/xpto`,
+  },
+]
