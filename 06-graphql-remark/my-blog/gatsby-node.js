3e9549ce58fedddbdc44e18816a54ec0c61918a8
Rafael Barros <rafaelbarros.df@gmail.com>
2020-06-24 02:57:04 -0300

    :construction: Usando o createPages da Gatsby Node API

--- 06-graphql-remark/my-blog/gatsby-node.js
+++ 06-graphql-remark/my-blog/gatsby-node.js
@@ -3,8 +3,8 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require('path')
 
- // To add the slug field to each post
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 // To add the slug field to each post
@@ -26,4 +26,31 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: `/${slug.slice(12)}`,
     })
   }
+}
+
+exports.createPages = ({graphql, actions}) => {
+    const { createPage } = actions
+    return graphql(`
+    {
+        allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug,
+              }
+            }
+          }
+        }
+      }    
+    `).then(result => {
+        result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+            createPage({
+                path: node.fields.slug,
+                component: path.resolve('./src/templates/blog-post.js'),
+                context: {
+                    slug: node.fields.slug
+                }
+            })
+        })
+    })
 }
