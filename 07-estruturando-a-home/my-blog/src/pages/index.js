698e57e4dcb1fa6267c2849f8a45b2f97011d2f7
Rafael Barros <rafaelbarros.df@gmail.com>
2020-06-24 20:54:57 -0300

    :construction: Ordenando os posts pela data

--- 07-estruturando-a-home/my-blog/src/pages/index.js
+++ 07-estruturando-a-home/my-blog/src/pages/index.js
@@ -8,13 +8,13 @@ import PostItem from '../components/PostItem'
 const IndexPage = () => {
 
   const { allMarkdownRemark } = useStaticQuery(graphql`
-    query PostList {
-        allMarkdownRemark {
+      query PostList {
+        allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
           edges {
             node {
               fields {
-                slug,
-              },
+                slug
+              }
               frontmatter {
                 background
                 category
