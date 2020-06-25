9e86cb8ccafbf7b65186a9bca83131165918cf69
Rafael Barros <rafaelbarros.df@gmail.com>
2020-06-25 09:56:40 -0300

    :truck: Movendo projeto

---
+++ 08-estruturando-a-pagina-post/my-blog/src/components/Profile/index.js
@@ -0,0 +1,36 @@
+import { graphql, useStaticQuery } from 'gatsby'
+import React from 'react'
+import Avatar from '../Avatar'
+import * as S from './styled'
+
+const Profile = () => {
+    const { 
+        site: {
+            siteMetadata: {title, position, description },
+        },
+    } = useStaticQuery(graphql`
+            query MySiteMetadata {
+                site {
+                    siteMetadata {
+                        title
+                        position
+                        description
+                    }
+                }
+            }
+        `)
+    return (
+        <S.ProfileWrapper>
+            <S.ProfileLink>
+            <Avatar />
+                <S.ProfileAuthor>
+                    {title}
+                    <S.ProfilePosition>{position}</S.ProfilePosition>
+                </S.ProfileAuthor>
+            </S.ProfileLink>    
+            <S.ProfileDescription>{description}</S.ProfileDescription>
+        </S.ProfileWrapper>
+    )     
+}
+
+export default Profile
