a07d3a9786f9198203e268620dcf6bedeabc22f3
Rafael Barros <rafaelbarros.df@gmail.com>
2020-06-30 20:34:51 -0300

    :truck: Movendo projeto

---
+++ 09-criando-a-pagina-de-search/my-blog/src/components/SocialLinks/content.js
@@ -0,0 +1,25 @@
+
+const links = [
+  {
+    label: "Github",
+    url: `https://github.com/rafaellbarros`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/_rafaelk99`,
+  },
+  {
+    label: "Youtube",
+    url: `https://www.youtube.com/xpto`,
+  },
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/_rafaelk99`,
+  },
+  {
+    label: "Unsplash",
+    url: `https://unsplash.com/xpto`,
+  },
+]
+
+export default links
