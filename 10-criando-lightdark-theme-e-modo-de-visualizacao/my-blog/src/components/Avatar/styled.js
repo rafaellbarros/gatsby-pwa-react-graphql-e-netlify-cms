3a54dce18d766c2edc23144b4d8e2bad611874de
Rafael Barros <rafaelbarros.df@gmail.com>
2020-07-01 12:33:51 -0300

    :truck: Movendo projeto

---
+++ 10-criando-lightdark-theme-e-modo-de-visualizacao/my-blog/src/components/Avatar/styled.js
@@ -0,0 +1,9 @@
+import Img from 'gatsby-image'
+import styled from 'styled-components'
+
+export const AvatarWrapper = styled(Img)`
+    border-radius: 50%;
+    height: 3.75rem;
+    margin: auto;
+    width: 3.75rem;
+`
